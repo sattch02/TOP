ccc98720be9c69f6f54c554d9fec8a408de30d8d
yasuo tatsumi <tatsumi@aa>
2022-05-15 05:38:59 +0900

    SystemManagerを作成

---
+++ Unity/Assets/System/Dialog/SystemDialog.cs
@@ -0,0 +1,133 @@
+﻿using System;
+#if PLATFORM_STANDALONE_WIN
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+#endif
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public enum SYSTEM_DIALOG_BUTTON_TYPE
+{
+    OK,
+    OKCancel,
+    YesNo,
+    RetryCancel,
+}
+
+public enum SYSTEM_DIALOG_BUTTON_RESULT
+{
+    None,
+    YesOrOk,
+    NoOrCancel,
+}
+
+/// <summary>
+/// システムのダイアログ
+/// </summary>
+public class SystemDialog
+{
+    /// <summary>
+    /// ダイアログを開く
+    /// </summary>
+    /// <param name="title"></param>
+    /// <param name="message"></param>
+    /// <param name="type"></param>
+    /// <param name="callback"></param>
+    public static void Open(string title, string message, SYSTEM_DIALOG_BUTTON_TYPE type, Action<SYSTEM_DIALOG_BUTTON_RESULT> callback)
+    {
+        SYSTEM_DIALOG_BUTTON_RESULT resultType = SYSTEM_DIALOG_BUTTON_RESULT.None;
+#if PLATFORM_STANDALONE_WIN
+        MessageBoxButtons messageBoxButtons;
+        switch (type)
+        {
+            default:
+            case SYSTEM_DIALOG_BUTTON_TYPE.OK:
+                messageBoxButtons = MessageBoxButtons.OK;
+                break;
+            case SYSTEM_DIALOG_BUTTON_TYPE.OKCancel:
+                messageBoxButtons = MessageBoxButtons.OKCancel;
+                break;
+            case SYSTEM_DIALOG_BUTTON_TYPE.YesNo:
+                messageBoxButtons = MessageBoxButtons.YesNo;
+                break;
+            case SYSTEM_DIALOG_BUTTON_TYPE.RetryCancel:
+                messageBoxButtons = MessageBoxButtons.RetryCancel;
+                break;
+        }
+
+        var result = MessageBox.Show(
+            text: message,
+            caption: title,
+            buttons: messageBoxButtons
+        );
+
+        switch (result)
+        {
+            case DialogResult.OK:
+            case DialogResult.Yes:
+            case DialogResult.Retry:
+                resultType = SYSTEM_DIALOG_BUTTON_RESULT.YesOrOk;
+                break;
+            case DialogResult.No:
+            case DialogResult.Cancel:
+                resultType = SYSTEM_DIALOG_BUTTON_RESULT.NoOrCancel;
+                break;
+            case DialogResult.None:
+            case DialogResult.Abort:
+            case DialogResult.Ignore:
+                //基本使わないもの
+                break;
+        }
+
+        if (callback != null)
+        {
+            callback.Invoke(resultType);
+        }
+#elif UNITY_EDITOR_OSX
+        // MacのEditorでは生成できないのでEditorUtilityで代用
+        string okName = "";
+        string cancelName = "";
+        switch (type)
+        {
+            default:
+            case SYSTEM_DIALOG_BUTTON_TYPE.OK:
+                okName = "OK";
+                break;
+            case SYSTEM_DIALOG_BUTTON_TYPE.OKCancel:
+                okName = "OK";
+                cancelName = "Cancel";
+                break;
+            case SYSTEM_DIALOG_BUTTON_TYPE.YesNo:
+                okName = "Yes";
+                cancelName = "No";
+                break;
+            case SYSTEM_DIALOG_BUTTON_TYPE.RetryCancel:
+                okName = "Retry";
+                cancelName = "Cancel";
+                break;
+        }
+        var result = EditorUtility.DisplayDialog(title: title, message: message, ok:okName, cancel: cancelName);
+
+        if (result)
+        {
+            resultType = SYSTEM_DIALOG_BUTTON_RESULT.YesOrOk;
+        }
+        else
+        {
+            resultType = SYSTEM_DIALOG_BUTTON_RESULT.NoOrCancel;
+        }
+        if (callback != null)
+        {
+            callback.Invoke(resultType);
+        }
+#elif UNITY_STANDALONE_OSX
+// plugin作るしかない
+#elif UNITY_IOS
+// plugin作るしかない
+#elif UNITY_ANDROID
+// plugin作るしかない
+#endif
+    }
+}
---
+++ Unity/Assets/System/Manager/SystemManager.cs
@@ -0,0 +1,53 @@
+﻿//#define DIALOG_IN_GAME_UI
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SystemManager : SingletonMonoBehaviour<SystemManager>
+{
+    static bool isAutoCreate = true;
+
+    /// <summary>
+    /// ゲーム起動時に自動生成
+    /// </summary>
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSplashScreen)]
+    static void AutoCreate()
+    {
+        if (!isAutoCreate) return;
+
+        if (instance == null)
+        {
+            // ゲーム中に常に存在するオブジェクトを生成、およびシーンの変更時にも破棄されないようにする。
+            var go = new GameObject(typeof(SystemManager).Name);
+            instance = go.AddComponent<SystemManager>();
+            GameObject.DontDestroyOnLoad(go);
+        }
+    }
+
+    /// <summary>
+    /// システムダイアログを開く
+    /// </summary>
+    /// <param name="message"></param>
+    /// <param name="title"></param>
+    /// <param name="type"></param>
+    /// <param name="callback"></param>
+    public static void OpenSystemDialog(string message, string title = "", SYSTEM_DIALOG_BUTTON_TYPE type = SYSTEM_DIALOG_BUTTON_TYPE.OK, Action<SYSTEM_DIALOG_BUTTON_RESULT> callback = null)
+    {
+#if DIALOG_IN_GAME_UI
+        // ゲーム内で作成するならdefineをonにしてその仕組みを作ってここに書く
+#else
+        SystemDialog.Open(title, message, type, callback);
+#endif
+    }
+
+    /// <summary>
+    /// ブラウザでURLを開く
+    /// </summary>
+    /// <param name="url"></param>
+    public static void OpenURL(string url)
+    {
+        Application.OpenURL(url);
+    }
+}
